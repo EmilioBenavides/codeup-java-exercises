ca9878400fc5f1e2be91dd8551c766ed4cd202da
Emilio Benavides <emilio.benavides68@gmail.com>
2022-03-01 16:41:31 -0600

    completed syntax, types, and variables exercises

---
+++ src/main/ConsoleExercises.java
@@ -0,0 +1,42 @@
+package main;
+
+import java.util.Scanner;
+
+public class ConsoleExercises {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double pi = 3.14159;
+        System.out.printf("Pie is: %.2f", pi);
+
+        System.out.print("Please enter an integer ");
+        int myInt = input.nextInt();
+
+        //When you enter a non-integer it throws an error
+
+        System.out.print("Enter 3 words: ");
+        String word1 = input.next();
+        String word2 = input.next();
+        String word3 = input.next();
+        System.out.printf("You entered: \n%s, \n%s, \n%s", word1, word2, word3);
+
+        input.nextLine();
+
+        System.out.print("enter a sentence: ");
+        String sentence = input.nextLine();
+        System.out.printf("You entered: %s\n", sentence);
+
+        System.out.print("enter a room length (int): ");
+        String lengthString = input.nextLine();
+        System.out.print("enter room width (int): ");
+        String widthString = input.nextLine();
+
+        int length = Integer.parseInt(lengthString);
+        int width = Integer.parseInt(lengthString);
+        int area = length * width;
+        int perimeter = (2 * length) + (2 * width);
+        System.out.printf("Area of the room: %f");
+
+
+    }
+}
--- src/main/HelloWorld.java
+++ src/main/HelloWorld.java
@@ -3,5 +3,75 @@ package main;
 public class HelloWorld {
     public static void main(String[] args) {
        System.out.println("Hello, World!");
+        int myFavoriteNumber = 3;
+        System.out.println(myFavoriteNumber);
+       String  myString = "This is my favorite number!";
+
+        System.out.println(myString);
+        System.out.println("3");
+
+//       myString = (String)'c'; // can't be used because myString is not a char
+//        myString = 3.14159; // It can't be use because a mysString is a string not a number.
+
+//        long myNumber;
+//        System.out.println(myNumber);  // does not work because it was not assigned a value
+//
+//        myNumber = 3.14; // Does not work because it is a double  type
+//
+//        myNumber = 123L;
+//
+//        long myNumber = 3.14; throws an error due to not being a solid integer
+
+        //int myNumber = 3.14; // throws an error due to it having a decimal point
+
+        //int myNumber = 123; // this works because it is a solid integer
+
+        //double myNumberTwo = 1.1;
+        float myNumberTwo = 1.1f; // this work due to having the f at the end of the value
+//
+//        int x = 5;
+//        System.out.println(x++); this code block increments after the printout
+//        System.out.println(x);
+
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);// This codeblock increments before printout
+
+        // class has predefined work used in JAVA
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;// you can't use this code block due to the three being string type of and vise versa the three as an int.
+
+       // int three = (int) "three"; // this is the same thing
+
+//        int x = 4;
+//       // x = x + 5;
+//        x += 5;
+//
+//        int x = 3;
+//        int y = 4;
+//       // y = y * x;
+//        y *= x;
+//
+//        int x = 10;
+//        int y = 2;
+//        x = x / y;
+//        y = y - x;
+//        x /= y;
+//        y -= x;
+
+        byte val = (byte) 128;
+        byte maxByte = Byte.MAX_VALUE;
+        System.out.println(val);
+        System.out.println(maxByte);
+
+
+
+
+
+
+
     }
 }
+
