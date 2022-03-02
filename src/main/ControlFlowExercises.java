63a211c47dd35c82c2726f3f2a70a8939a9a1990
Emilio Benavides <emilio.benavides68@gmail.com>
2022-03-02 13:52:27 -0600

    Completed no1 StringExercise

--- src/main/ControlFlowExercises.java
+++ src/main/ControlFlowExercises.java
@@ -6,10 +6,49 @@ public class ControlFlowExercises {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int i = 5;
-        while (i <= 15) {
-            System.out.println("i is " + i);
-            i++;
+//        int i = 5;
+//        while (i <= 15) {
+//            System.out.print(i + " ");
+//            i++;
+//
+//        }
+//
+//        int i = 0;
+//        do {
+//            System.out.println(i);
+//            i += 2;
+//        } while (i <= 100);
+//    }
+
+//        int i = 100;
+//
+//        do {
+//            System.out.println(i);
+//            i -= 5;
+//        } while(i >= -10);
+//        }
+
+//        long i = 2L;
+//
+//        do {
+//            System.out.println(i);
+//            i *= i;
+//        } while (i <1000000L);
+
+//        for (int i = 100; i>= -10; i-=5) {
+//            System.out.println(i);
+//        }
+//        for (long i = 2L; i < 1000000L; i*=i) {
+//            System.out.println(i);
+//        }
+
+        for (int i = 1; i< 100; i++) {
+            if (i% 3 == 0 && i % 5 ==0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 ==0);
         }
     }
+
 }
+
+
---
+++ src/main/StringExercise.java
@@ -0,0 +1,20 @@
+package main;
+
+import java.util.Scanner;
+
+public class StringExercise {
+    public static void main(String[] args) {
+//
+//        String  strgInput = "We don't need no education\nWe don't need no thought control";
+//
+//                System.out.print(strgInput);
+//
+//        String strgInput = "Check \"this\" out!, \"s inside of \"s!";
+//        System.out.println(strgInput);
+//
+//
+//        String strgInput = "I can do backslashes \\, double backslashes \\\\, and the amazing triple backslash \\\\\\!";
+//        System.out.println(strgInput);
+//
+//    }
+//}
