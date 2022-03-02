09c53e67e396da7e38010bf51304f9efe3e27b03
Emilio Benavides <emilio.benavides68@gmail.com>
2022-03-02 10:39:09 -0600

    Completed 1 exercise

---
+++ src/main/ControlFlowExercises.java
@@ -0,0 +1,15 @@
+package main;
+
+import java.util.Scanner;
+
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int i = 5;
+        while (i <= 15) {
+            System.out.println("i is " + i);
+            i++;
+        }
+    }
+}
