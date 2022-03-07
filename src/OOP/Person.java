e021041c2540d86c53e2c3d14b242a3f5c6a13cd
Emilio Benavides <emilio.benavides68@gmail.com>
2022-03-07 10:22:40 -0600

    Completed exercise 2

--- src/OOP/Person.java
+++ src/OOP/Person.java
@@ -2,6 +2,9 @@ package OOP;
 
 public class Person {
     private String name;  // this is an instance variable
+    public Person(String name) {
+        this.name = name;
+    }
 
     public String getName(){
 //TODO: return the person's name
@@ -18,11 +21,24 @@ public class Person {
     }
 
     public static void main(String[] args) {
-        Person person1 = new Person();  // This is the new "object" created that inherits all four of the properties of the Person class above. To access any of the public properties
-                                        // you would need to call objectname.property ex. person1.getname
-        System.out.println(person1.getName());
-        person1.setName("Tony");
-        System.out.println(person1.getName());
-        person1.sayHello();
+//        Person person1 = new Person();  // This is the new "object" created that inherits all four of the properties of the Person class above. To access any of the public properties
+//                                        // you would need to call objectname.property ex. person1.getname
+//        System.out.println(person1.getName());
+//        person1.setName("Tony");
+//        System.out.println(person1.getName());
+//        person1.sayHello();
+
+//        Person person1 = new Person("John");  // Person1 and Person2 are not the equal to each other because they take up 2 different memory slots
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName()));
+//        System.out.println(person1 == person2); // this is true because person2 was not previously assigned to anything
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1.getName()); // "John"
+//        System.out.println(person2.getName()); // "John"
+//        person2.setName("Jane");
+//        System.out.println(person1.getName()); // look at line #37 this is now assigned the name "Jane"
+//        System.out.println(person2.getName()); // they are both assigned the name "Jane" because they have the same memory slot.
     }
 }
