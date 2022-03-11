d58d2f2cfb8ec9be6fb411c62f319cc8b10c2160
Emilio Benavides <emilio.benavides68@gmail.com>
2022-03-11 10:00:14 -0600

    Completed ServerNameGenerator exercise

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,14 +1,22 @@
 public class ServerNameGenerator {
     public static void main(String[] args) {
         // Create two arrays whose elements are strings: one with at least 10 adjectives, another with at least 10 nouns.
-        String[] adjectives;
-        String[] nouns;
-        adjectives = new String[10];
-        nouns = new String[10];
+        String[] adjectives = {"Confused", "Hungry", "Thirsty", "Bamboozle", "Bodacious", "Brouhaha", "Canoodle", "Gnarly", "Malarkey", "Nincompoop"};
+        String[] nouns = {"Horse", "Car", "Chicken", "Truck", "Texas", "Alaska", "Kentucky", "Delaware", "Mouse", "New York"};
 
-        //Create a method that will return a random element from an array of strings
-
-
-
+        String adjective = pickRandomString(adjectives);
+        String noun = pickRandomString(nouns);
+        System.out.println("Here is your server name: \n" + pickRandomString(nouns) + "-" + pickRandomString(adjectives));
     }
+
+        //Create a method that will return a random element from an array of strings.
+        //Add a main method, and inside of your main method select and random noun and adjective and hyphenate the
+        // combination and display the generated name to the user.
+        public static String pickRandomString(String[] stringArray) {
+        int index = (int) (Math.random() * stringArray.length);
+        return stringArray[index];
+        }
+
+
+
 }
